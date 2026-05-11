2e913500ade2fc91607548bf186d0388d75b674d
PushpenderY <pushpender200406@gmail.com>
2026-05-11 09:36:45 +0530

    learn routes working or not through postman

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 app.use(cookieParser());
@@ -14,4 +15,16 @@ app.use(express.json({limit: "16kb"})); // Middleware to parse JSON request bodi
 app.use(express.urlencoded({extended: true, limit: "16kb"})); // Middleware to parse URL-encoded request bodies
 app.use(express.static("public")); // Middleware to serve static files from the "public" directory
 
-export default app;
+//Test Route
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to Chai Aur Backend",
+    });
+});
+//route decleration
+app.use("/api/v1/users", userRouter);
+
+// http:localhost:8000/api/v1/users/register
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,69 @@
-//require("dotenv").config({path: "./.env"}); //provides access to environment variables defined in .env file as early as possible in the application lifecycle
-
-import dotenv from "dotenv";
-
-import mongoose from "mongoose";
-import{ DB_NAME } from "./constants.js";
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 
-import express from "express";
-const app = express();
 
-dotenv.config({path: "./.env"});
 
 connectDB()
-.then(  () => {
-    app.listen(process.env.PORT || 8000, () =>{
-        console.log(`App is listening on port ${process.env.PORT || 8000}`);
-    });
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
-.catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-});
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//require("dotenv").config({path: "./.env"}); //provides access to environment variables defined in .env file as early as possible in the application lifecycle
+
+// import dotenv from "dotenv";
+
+// import mongoose from "mongoose";
+// import{ DB_NAME } from "./constants.js";
+// import connectDB from "./db/index.js";
+// // import { app } from "./app.js";
+
+// import express from "express";
+// const app = express();
+
+// dotenv.config({path: "./.env"});
+
+// connectDB()
+// .then(  () => {
+//     app.listen(process.env.PORT || 8000, () =>{
+//         console.log(`App is listening on port ${process.env.PORT || 8000}`);
+//     });
+// })
+// .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//     process.exit(1);
+// });
 
 
 
