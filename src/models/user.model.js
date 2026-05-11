ed91445ff553921a6b34072d8f8017201d7c4c45
PushpenderY <pushpender200406@gmail.com>
2026-05-11 18:05:46 +0530

    learn postman and upload data into database viva postman and code

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,10 +46,13 @@ const userSchema = new Schema({
     }
 }, {timestamps: true});
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(
+        this.password,
+        10
+    );
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -71,20 +74,6 @@ userSchema.methods.generateAccessToken = function () {
     );
 };
 
-user.schema.methods.generateAccessToken = function () {
-        return jwt.sign(
-            {
-                _id: this._id,
-                email: this.email,
-                username: this.username,
-                fullname: this.fullname
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            {
-                expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-            }
-        );
-};
 userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,28 @@
-import {v2 as cloudinary} from 'cloudinary';
-import fs from 'fs';
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath)return null;
-        // Upload the file to Cloudinary
+        if (!localFilePath) return null
+        //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto",
+            resource_type: "auto"
         })
-        //file is uploaded successfully
-        console.log("File uploaded successfully", response.url);
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
+
     } catch (error) {
-        fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
-};
-
-// cloudinary.v2.uploader.upload("https://res.cloudinary.com/djlkkhu8o/image/upload/q_auto/f_auto/v1778416726/cld-sample-5.jpg",
-//     {public_id: "cld-sample-5"}, 
-//     function(error, result) {console.log(result);
-// });
+}
 
 export {uploadOnCloudinary};
